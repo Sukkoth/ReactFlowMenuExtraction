6ca38a3fa47920ae0f523d130795482ff1df4c74
Sukkoth <suukootj@gmail.com>
2024-04-19 21:59:41 +0300

    custom menu

--- src/components/AppNode.tsx
+++ src/components/AppNode.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Handle, Position } from "reactflow";
 
 type propTypes = {
@@ -5,22 +6,43 @@ type propTypes = {
   id: string;
 };
 
-function AppNode({ data }: propTypes) {
+function AppNode({ data, id }: propTypes) {
+  const [showMenu, setShowMenu] = useState<boolean>(false);
   return (
-    <div className='px-5 py-2 border-2 rounded-2xl border-orange-200 min-w-[10rem] bg-white text-center z-10 shadow-xl'>
-      <p>{data.label}</p>
-      <Handle
-        type='target'
-        position={Position.Top}
-        id='a'
-        isConnectable={true}
-      />
-      <Handle
-        type='source'
-        position={Position.Bottom}
-        id='b'
-        isConnectable={true}
-      />
+    <div
+      onMouseEnter={() => setShowMenu(true)}
+      onMouseLeave={() => setShowMenu(false)}
+      className='flex border-2 p-1 border-orange-200 min-w-[10rem] bg-white text-center z-10 shadow-xl'
+    >
+      <div className='w-[95%]'>
+        <p className='px-3 py-2'>{data.label}</p>
+        <Handle
+          type='target'
+          position={Position.Top}
+          id='a'
+          isConnectable={true}
+        />
+        <Handle
+          type='source'
+          position={Position.Bottom}
+          id='b'
+          isConnectable={true}
+        />
+      </div>
+
+      <div
+        className={` absolute right-1 top-1 bottom-1 w-[10%] flex flex-col justify-between `}
+        style={{
+          visibility: showMenu ? "visible" : "hidden",
+        }}
+      >
+        <button onClick={() => console.log("DUPLICATE ", id)}>
+          <img src='/copy.png' alt='Copy this node' className='size-4' />
+        </button>
+        <button onClick={() => console.log("DELETE ", id)}>
+          <img src='/trash1.png' alt='Copy this node' className='size-4' />
+        </button>
+      </div>
     </div>
   );
 }
